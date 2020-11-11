21d6f3114c3ca6038d885115e2b4c44483b446d2
Beau Jennings <redalthazar@gmail.com>
2020-11-11 15:21:57 -0600

    finished tasks 1 and 2, need to look back at task2 stretch questions

--- index.js
+++ index.js
@@ -39,9 +39,31 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function (edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person('Julio', 17);
+const personTwo = new Person('Cameron', 26);
+const personThree = new Person('George', 49);
+
+
 
 /*
   TASK 2
@@ -57,9 +79,21 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  if(this.tank< 100){
+    this.tank = gallons + this.tank
+  };
+}
+
+
+
+
 
 /*
   TASK 3
